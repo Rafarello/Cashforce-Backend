f1753d7ce80e1b27e998020e07f6edf3dedbf079
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-27 19:55:21 -0300

    Feat: Utilizando tratativa de erro em Controller

--- src/controllers/BuyersController.js
+++ src/controllers/BuyersController.js
@@ -1,14 +1,16 @@
 const httpStatusCode = require("../enums/httpStatusCode");
-const tableStatusCode = require("../enums/tableStatusCode");
 const BuyersService = require("../services/BuyersService");
 
 
 class BuyersController {
-  static async getBuyersInfo(_req, res) {
-    const response = await BuyersService.getBuyersInfo();
+  static async getBuyersInfo(_req, res, next) {
+    try {
+      const response = await BuyersService.getBuyersInfo();
 
-    console.log(tableStatusCode[8])
-    return res.status(httpStatusCode.OK).json(response);
+      return res.status(httpStatusCode.OK).json(response);
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
