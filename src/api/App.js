e7ae89cf108764bd21b52605d6800fea2725477c
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-27 19:41:20 -0300

    Feat: Criando App

---
+++ src/api/App.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+
+// App feito de acordo com o que foi feito no Projeto Trybe Futebol Clube - Rafael
+// Única mudança foi construir ele em Javascript e não em Typescript
+
+class App {
+  constructor() {
+    this.app = express();
+    this.config();
+    this.app.use(express.json());
+    this.app.use(express.static('public'));
+  }
+
+  // Função config serve para determinar os controles de acessos autorizados para nosso projeto,
+  // inicialmente com os mesmos utilizados no TFC
+
+  config() {
+    const accessControl = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+    this.app.use(cors());
+    this.app.get('/', (_request, response) => {
+      response.send();
+    });
+  }
+
+  addRouter(router) {
+    this.app.use(router);
+  }
+
+  start(PORT) {
+      this.app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+  } 
+}
+
+module.exports = App;
